9d3df66677250a616a819f2c69691166b4c0e826
GEO-HACK <geoffreykirumba0@gmail.com>
2024-12-19 13:32:04 +0300

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <div>
+      <Link href="#">
+      <h1 className="text-5xl font-bold">THis is the home of champions</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+        cumque aliquam facere eaque labore id ea est quisquam expedita assumenda
+        amet illo aut unde! Nulla labore officia maxime odit aliquid.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+        cumque aliquam facere eaque labore id ea est quisquam expedita assumenda
+        amet illo aut unde! Nulla labore officia maxime odit aliquid.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+        cumque aliquam facere eaque labore id ea est quisquam expedita assumenda
+        amet illo aut unde! Nulla labore officia maxime odit aliquid.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+        cumque aliquam facere eaque labore id ea est quisquam expedita assumenda
+        amet illo aut unde! Nulla labore officia maxime odit aliquid.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+        cumque aliquam facere eaque labore id ea est quisquam expedita assumenda
+        amet illo aut unde! Nulla labore officia maxime odit aliquid.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+        cumque aliquam facere eaque labore id ea est quisquam expedita assumenda
+        amet illo aut unde! Nulla labore officia maxime odit aliquid.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+        cumque aliquam facere eaque labore id ea est quisquam expedita assumenda
+        amet illo aut unde! Nulla labore officia maxime odit aliquid.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+        cumque aliquam facere eaque labore id ea est quisquam expedita assumenda
+        amet illo aut unde! Nulla labore officia maxime odit aliquid.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+        cumque aliquam facere eaque labore id ea est quisquam expedita assumenda
+        amet illo aut unde! Nulla labore officia maxime odit aliquid.
+      </Link>
+    </div>
+  );
+};
+
+export default page;
