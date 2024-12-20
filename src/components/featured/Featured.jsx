3eed593b534acaef44f0c2a889a4f45f899275a2
GEO-HACK <geoffreykirumba0@gmail.com>
2024-12-20 15:43:59 +0300

    the featured page

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Featured = () => {
   return (
-    <div>Featured</div>
-  )
-}
+    <div className="mt-[30px]">
+      <h1 className="text-[50px] ">
+        <b>Hello devs !! </b> Discover my stories and new Creative  ideas in the tech industry
+      </h1>
+      <div className="flex mt-[30px] gap-[50px] ">
+        <div className="relative flex-1 h-[450px]">
+          <Image src="/p1.jpeg" alt="thi is the features product" fill  className="object-cover"/>
+        </div>
+        <div className="flex-1 flex flex-col gap-[20px] items-start justify-center">
+      
+          <h1 className="text-[35px] font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing</h1>
+          <p className="text-md font-[300px] text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit at
+            dolor et earum doloremque, numquam cum obcaecati iste aspernatur
+            impedit. Porro est nisi nobis, ad facilis repellat doloremque fuga
+            sed.
+          </p>
+          <button className="px-5 py-2 bg-black text-white rounded-md max-w-max">ReadMore</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Featured
+export default Featured;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,84 @@
-import React from 'react'
-import Image from 'next/image'
-import  Link  from 'next/link'
-import ThemeToggle from '../themeToggle/ThemeToggle'
-import AuthLinks from '../authLinks/AuthLinks'
+"use client"
+
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import ThemeToggle from "../themeToggle/ThemeToggle";
+import AuthLinks from "../authLinks/AuthLinks";
 
 const Navbar = () => {
-  return (
-    <div className='flex-1 flex justify-between h-[100px] items-center'>
-        <div className=' flex gap-2 flex-1'>
-            <Image src="/facebook.png" alt='images of socials' width={24} height={24}/>
-            <Image src="/instagram.png" alt='images of socials' width={24} height={24}/>
-            <Image src="/tiktok.png" alt='images of socials' width={24} height={24}/>
-            <Image src="/youtube.png" alt='images of socials' width={24} height={24}/>
-            
-        </div>
-        <div className='flex-1 font-bold text-[36px] text-center'>Scribbly</div>
-        <div className='flex-1 flex gap-3 px-2  text-sm font-semibold '>
-            <ThemeToggle/>
-            <Link href="/">HomePage</Link>
-            <Link href="/contact">Contact</Link>
-            <Link href="/about">About</Link>
-            <AuthLinks/>
-        </div>
-           
-    </div>
-  )
-}
+    const [isMenuopen, setIsMenuopen] = useState(false)
 
-export default Navbar
+    const toggleMenu = ()=>{
+        setIsMenuopen(!isMenuopen)
+    }
+
+    const closeMenu = ()=>
+    {
+        setIsMenuopen(false)
+    }
+  return (
+    <div className="flex-1 flex justify-between h-[100px] items-center">
+      {/* Social Handles */}
+      <div className="flex gap-2 flex-1 hidden sm:flex">
+        <Image
+          src="/facebook.png"
+          alt="Facebook logo"
+          width={24}
+          height={24}
+        />
+        <Image
+          src="/instagram.png"
+          alt="Instagram logo"
+          width={24}
+          height={24}
+        />
+        <Image src="/tiktok.png" alt="TikTok logo" width={24} height={24} />
+        <Image src="/youtube.png" alt="YouTube logo" width={24} height={24} />
+      </div>
+
+      {/* Brand Name */}
+      <div className="flex-1 font-bold text-[36px] text-center">Scribbly</div>
+
+      {/* Navigation Links and Theme Toggle */}
+      <div className="flex-1 flex gap-3 px-2 text-sm font-semibolda">
+        <ThemeToggle />
+
+        {/* burger menu */}
+        <button 
+        onClick={toggleMenu}
+        className="block sm:hidden text-xl focus:outline-none"
+        >
+            ☰ 
+        </button>
+        <div
+          className={`absolute top-[100px] left-0 w-full bg-background shadow-md p-4 sm:static sm:bg-transparent sm:shadow-none sm:p-0 sm:flex sm:gap-2 ${
+            isMenuopen ? "block" : "hidden"
+          }`}
+        >
+          <div className="flex flex-col sm:flex-row sm:gap-4">
+            <Link
+            onClick={closeMenu}
+             href="/" className="block py-2 sm:py-0">
+              HomePage
+            </Link>
+            <Link
+            onClick={closeMenu}
+
+             href="/contact" className="block py-2 sm:py-0">
+              Contact
+            </Link>
+            <Link
+            onClick={closeMenu}
+             href="/about" className="block py-2 sm:py-0">
+              About
+            </Link>
+            <AuthLinks />
+          </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
