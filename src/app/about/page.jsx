2ba5d3d8f479d6664d89fa247e9b95c4e7fc78a3
GEO-HACK <geoffreykirumba0@gmail.com>
2025-01-28 16:28:12 +0300

    about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <div className="relative bg-white h-[500px]">
+        <Image
+          src="/about.avif"
+          alt="this is the featured product"
+          fill
+          className="absolute object-cover rounded-lg opacity-50"
+        />
+        {/* Centering Text */}
+        <div className=" flex-col absolute inset-0 flex mt-20 p-3 justify-start">
+          <h1 className="text-black text-5xl font-bold ">
+            SCRIBBLY: Where ideas take flight
+          </h1>
+          <div className="flex flex-row gap-4 mt-4">
+            <button className="text-md  bg-orange-500 px-4 py-3">
+              Start scribbling
+            </button>
+            <button className="text-md  bg-orange-500 px-4 py-3">
+              {" "}
+              Explore the community
+            </button>
+          </div>
+        </div>
+      </div>
 
-export default page
+      {/* second section */}
+      <div className="flex items-center bg-orange-500 h-[600px]">
+        <div className=" flex flex-col bg-white h-[350px] w-[90%] mx-auto p-6">
+          <h1 className="text-4xl font-semibold">About Scribbly</h1>
+          <div className="flex flex-row gap-4 mt-5 px-6 justify-between">
+            <div className="flex flex-col w-[40%]">
+              <h1 className=" text-xl font-semibold">
+                A Canvas for Your Thoughts
+              </h1>
+              <p className="text-gray-500 mt-2 text-lg leading-relaxed ">
+                Scribbly is a platform where anyone can share their ideas,
+                thoughts, and stories with the world. Whether you're a seasoned
+                writer or just starting out, Scribbly provides a welcoming and
+                supportive environment for you to express yourself.
+              </p>
+            </div>
+            <div className="flex flex-col w-[40%]">
+              <h1 className="text-xl font-semibold">A community of Thinkers</h1>
+              <p className="text-gray-500 mt-2 text-lg leading-relax">
+                Our community is a diverse group of individuals who share a
+                passion for creative expression. Connect with like-minded
+                people, share your work, and receive feedback on your ideas.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Third section */}
+
+      <div className="flex flex-row h-[550px] justify-between ">
+        <div className="flex flex-col gap-4 w-[50%] p-6">
+          <div className="flex flex-row gap-4">
+            <div className="flex flex-col bg-gray-200 p-3">
+              <h1 className="text-xl font-semibold text-stone-500">Empowering Voices</h1>
+              <p className="text-gray-500 mt-2 text-lg leading-relaxed">
+                We believe everyone has something valuable to share. We aim to
+                empower individuals to find their voice and connect with others
+                who share their interests.
+              </p>
+            </div>
+            <div className="flex flex-col bg-gray-200 p-3">
+              <h1 className="text-xl font-semibold text-stone-500">Cultivating Creativity</h1>
+              <p className="text-gray-500 mt-2 text-lg leading-relaxed">
+                Scribbly provides a space for exploration, experimentation, and
+                growth. We want to foster a culture of creativity and inspire
+                others to pursue their passions.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex flex-col bg-gray-200 p-3">
+            <h1 className="text-xl font-semibold text-stone-500 ">Building Community</h1>
+            <p className="text-gray-500 mt-2 text-lg leading-relaxed">
+              We strive to create a welcoming and inclusive environment where
+              people can engage in meaningful conversations and build lasting
+              connections.Links HomePage Blog
+            </p>
+          </div>
+        </div>
+        <div className="relative bg-white h-[500px]">
+          <Image
+            src="/mission.jpg"
+            alt="this is missions image"
+            width={400}
+            height={400}
+          />
+        </div>
+      </div>
+
+      {/* fourth section */}
+      <div className="flex items-center bg-orange-500 h-[600px]">
+        <div className=" flex flex-col bg-white h-[350px] w-[90%] mx-auto p-6">
+          <h1 className="text-3xl font-semibold">How Scribbly works</h1>
+         
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
