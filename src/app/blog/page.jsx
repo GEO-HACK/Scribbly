9039f19b71ab316a9bb71c12982c1c6f5984295e
GEO-HACK <geoffreykirumba0@gmail.com>
2025-01-31 08:01:31 +0300

    image optimization

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -10,7 +10,7 @@ const BlogPage = ({ searchParams }) => {
   return (
     <div>
       <h1 className='w-full bg-orange-400 py-[5px] px-[10px] flex justify-center text-lg font-semibold'>
-        Style Blog
+        {cat} Blog
       </h1>
 
       <div className='flex gap-[50px] mt-6'>
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,34 +1,39 @@
 import React from "react";
-import Image from "next/image"; 
+import Image from "next/image";
 import Link from "next/link";
 
-const Card = ( {item}) => {
+const Card = ({ item }) => {
   return (
-    <div className="flex gap-[50px] mb-[50px] items-center" >
+    <div className="flex gap-[50px] mb-[50px] items-center">
       <div className="flex-1 relative w-auto h-[350px] hidden lg:block">
-        <Image
-          src="/p1.jpeg"
-          alt="this is the post image"
-          fill
-          className="absolute w-full h-full"
-        />
+        {item.img && (
+          <Image
+            src={item.img || "/placeholder.jpg"}
+            alt="this is the post image"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+            className="absolute w-full h-full"
+          />
+        )}
       </div>
       <div className="flex-1 flex flex-col gap-[30px]">
         <div className="">
-          <span className="text-gray-400">{item.createdAt.substring(0,10)}</span>
-          <span className="text-red-600 font-semibold"> - {item.catSlug.toUpperCase()}</span>
+          <span className="text-gray-400">
+            {item.createdAt.substring(0, 10)}
+          </span>
+          <span className="text-red-600 font-semibold">
+            {" "}
+            - {item.catSlug.toUpperCase()}
+          </span>
         </div>
         <Link href="/">
-        <h1 className="font-bold text-xl" >{item.title}</h1>
-  
+          <h1 className="font-bold text-xl">{item.title}</h1>
         </Link>
-        <p className="font-[18px] font-md text-gray">
-          {item.desc} 
-        </p>
-        <Link
-         href="#" 
-         className="border-b border-red-500 max-w-max pt-2 pb-1" 
-        >ReadMore</Link>
+        <p className="font-[18px] font-md text-gray">{item.desc}</p>
+        <Link href="#" className="border-b border-red-500 max-w-max pt-2 pb-1">
+          ReadMore
+        </Link>
       </div>
     </div>
   );
