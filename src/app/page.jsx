7e45530d7883bc600c61471783731657eacf5307
GEO-HACK <geoffreykirumba0@gmail.com>
2025-02-16 13:10:22 +0300

    searchparams

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import Featured from "@/components/featured/Featured";
 import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/menu/Menu";
+import {suspense } from "react";
 
 const Page = ({ searchParams }) => {
   const page = parseInt(searchParams?.page || "1", 10);
@@ -12,8 +13,14 @@ const Page = ({ searchParams }) => {
       <Featured />
       <CategoryList />
       <div className="flex gap-[50px] mt-6">
-        <CardList page={page} cat={cat} />
+        <suspense fallback={<div>Loading...</div>}>
+
+        <CardList page={page} cat={cat} /> 
+        </suspense>
+        <suspense fallback={<div>Loading menu...</div>}>
         <Menu />
+
+        </suspense>
       </div>
     </div>
   );
